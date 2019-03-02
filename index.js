631c653a9d42be440f5e96d28314ca48b2849b2d
kant003 <gonzalezm.angel@gmail.com>
2019-03-02 08:40:48 +0100

    cambios

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use( (req, res, next) => {
 const PiCamera = require('pi-camera');
 const myCamera = new PiCamera({
   mode: 'photo',
-  output: `${ __dirname }/test.jpg`,
+  output: `test.jpg`,
   width: 640,
   height: 480,
   nopreview: true,
@@ -52,6 +52,7 @@ myCamera.snap()
   })
   .catch((error) => {
      // Handle your error
+	console.log(error)
   });
 
 /*var T = new Twit(config);
